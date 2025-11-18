0d7cc70b040c221ce0d6bb6b9cfd030f757ca325
etm <marques@oriontec.com.br>
2025-11-17 21:57:08 -0300

    Ajuste para compilar o código atualizado com as alterações do repositório original

--- src/RESTRequest4D.Request.Client.pas
+++ src/RESTRequest4D.Request.Client.pas
@@ -342,8 +342,10 @@ begin
   FHeaders := TStringList.Create;
   FRESTRequest.OnAfterExecute := DoAfterExecute;
   FRESTRequest.OnHTTPProtocolError := DoHTTPProtocolError;
+  {$IFDEF FPC}
   FRESTClient.OnReceiveData := DoReceiveProgress;
   FRESTClient.OnSendData := DoSendProgress;
+  {$ENDIF}
   DoJoinComponents;
   FRESTClient.RaiseExceptionOn500 := False;
   FRetries := 0;
